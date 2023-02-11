2ae8fa20cc8e92a94d268bc0abead6241540b490
Luca Bracchi <luca.bracchi3@studio.unibo.it>
2023-02-11 15:05:51 +0100

    feat(auth): configured jwt authentication
    configured ktor jwt authentication for protected APIs

--- src/main/kotlin/com/example/plugins/Authentication.kt
+++ src/main/kotlin/com/example/plugins/Authentication.kt
@@ -1,33 +1,44 @@
 package com.example.plugins
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import com.example.user.routing.exposedUserRoutes
 import com.example.user.routing.protectedUserRoutes
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureAuthentication() {
 
+    val tokenSecret = "dSgUkXp2s5v8y/B?E(H+MbQeThWmYq3t"
+
     install(Authentication) {
 
-        basic("auth-basic") {
-            realm = "Access to the '/' path"
-            validate { credentials ->
-                if (credentials.name == "luca.bracchi" && credentials.password == "test123") {
-                    print("success validation")
-                    UserIdPrincipal(credentials.name)
-                } else {
+        jwt("auth-jwt") {
+
+            realm = "Parking System Backend"
+
+            verifier(JWT
+                .require(Algorithm.HMAC256(tokenSecret))
+                .build()
+            )
+
+            validate { credential ->
+                if(credential.payload.getClaim("email").asString() != "")
+                    JWTPrincipal(credential.payload)
+                else
                     null
-                }
             }
+
         }
 
     }
 
     routing {
 
-        authenticate("auth-basic") {
+        authenticate("auth-jwt") {
 
             get("/auth-test") {
                 call.respondText("Hello, ${call.principal<UserIdPrincipal>()?.name}!")
