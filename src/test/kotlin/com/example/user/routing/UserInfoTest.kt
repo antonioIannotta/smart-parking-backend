7c21aefd2921f872770e6b86809223934a37fe36
Luca Bracchi <luca.bracchi3@studio.unibo.it>
2023-02-16 19:03:26 +0100

    test: completed user info test
    added user/info api call in user info test

--- src/test/kotlin/com/example/user/routing/UserInfoTest.kt
+++ src/test/kotlin/com/example/user/routing/UserInfoTest.kt
@@ -3,6 +3,12 @@ package com.example.user.routing
 import com.example.user.controller.UserController
 import com.example.user.model.request.SignInRequestBody
 import com.example.user.model.request.SignUpRequestBody
+import com.example.user.model.response.UserInfoResponseBody
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
@@ -14,10 +20,10 @@ class UserInfoTest {
     //BEGIN: config
     companion object {
 
-        private val testMail = "test@test.it"
-        private val testPassword = "Test123!"
-        private val testName = "testName"
-        private val testSurname = "testSurname"
+        private const val testMail = "test@test.it"
+        private const val testPassword = "Test123!"
+        private const val testName = "testName"
+        private const val testSurname = "testSurname"
         private lateinit var userController: UserController
 
         @BeforeAll
@@ -30,7 +36,6 @@ class UserInfoTest {
                 val databaseName = environment.config.property("mongo.database.name").getString()
                 val collectionName = environment.config.property("mongo.database.collections.user").getString()
                 userController = UserController(mongoAddress, databaseName, collectionName, tokenSecret)
-
                 //register test user
                 val signUpRequestBody = SignUpRequestBody(testMail, testPassword, testName, testSurname)
                 userController.signUp(signUpRequestBody)
@@ -48,13 +53,24 @@ class UserInfoTest {
 
     @Test
     fun `test that user info return info about the user`() = testApplication {
-        //register user
+
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        //log user and get jwt
         val signInRequestBody = SignInRequestBody(testMail, testPassword)
-        userController.signIn(signInRequestBody)
+        val jwt = userController.signIn(signInRequestBody).token
         //get user info
-        val userInfoResponseBody = userController.userInfo(testMail)
-        //user info verification
-        assertEquals(userInfoResponseBody.userInfo?.email ?: "", testMail)
+        client.get("/user/info") {
+            header(HttpHeaders.Authorization, "Bearer $jwt")
+        }.apply {
+            val responseBody = call.response.body<UserInfoResponseBody>()
+            //user info verification
+            assertEquals(responseBody.userInfo?.email ?: "", testMail)
+        }
     }
 
 }
