f39e39da374fa69e7f4402b772d16a9e9fe321c0
antonioIannotta <74740099+antonioIannotta@users.noreply.github.com>
2023-02-17 23:51:49 +0100

    fix: added test

--- src/test/kotlin/com/example/database/DatabaseTest.kt
+++ src/test/kotlin/com/example/database/DatabaseTest.kt
@@ -1,7 +1,9 @@
 package com.example.database
 
 import com.example.parkingSlot.database.Database
+import com.example.parkingSlot.models.IncrementOccupation
 import com.example.parkingSlot.models.ParkingSlot
+import com.example.parkingSlot.models.SlotOccupation
 import io.ktor.server.http.content.*
 import kotlin.test.Test
 import java.time.LocalDateTime
@@ -45,4 +47,52 @@ class DatabaseTest {
         val slotId2 = "Z7"
         assertEquals(false, Database.isParkingSlotValid(slotId2, parkingSlotList))
     }
+
+    @Test
+    fun occupyTestTrue() {
+        val parkingSlotTestCollection = "parking-slot-test"
+        val parkingSlotList = Database.getAllParkingSlots(parkingSlotTestCollection)
+        val slotOccupation = SlotOccupation("C2", LocalDateTime.now().plusHours(3).toString())
+
+        val returnValue = Database.occupySlot(parkingSlotTestCollection, slotOccupation, parkingSlotList)
+        assertEquals(true, returnValue)
+    }
+
+    @Test
+    fun occupyTestFalse() {
+        val parkingSlotTestCollection = "parking-slot-test"
+        val parkingSlotList = Database.getAllParkingSlots(parkingSlotTestCollection)
+        val slotOccupation = SlotOccupation("C2", LocalDateTime.now().plusHours(3).toString())
+
+        val returnValue = Database.occupySlot(parkingSlotTestCollection, slotOccupation, parkingSlotList)
+        assertEquals(false, returnValue)
+    }
+
+    @Test
+    fun incrementOccupationTestTrue() {
+        val parkingSlotTestCollection = "parking-slot-test"
+        val parkingSlotList = Database.getAllParkingSlots(parkingSlotTestCollection)
+        val parkingSlot = Database.getParkingSlot(parkingSlotTestCollection, "C2")
+        val newEndStop = LocalDateTime.parse(parkingSlot.endStop).plusHours(2).toString()
+        val incrementOccupation = IncrementOccupation(parkingSlot.id, newEndStop)
+
+        val returnValue = Database.incrementOccupation(parkingSlotTestCollection, incrementOccupation, parkingSlotList)
+
+        assertEquals(true, returnValue)
+    }
+
+    @Test
+    fun incrementOccupationTestFalse1() {
+        val parkingSlotTestCollection = "parking-slot-test"
+        val parkingSlotList = Database.getAllParkingSlots(parkingSlotTestCollection)
+        val parkingSlot = Database.getParkingSlot(parkingSlotTestCollection, "C2")
+
+        val newEndStop = LocalDateTime.parse(parkingSlot.endStop).minusHours(2).toString()
+        val incrementOccupation = IncrementOccupation(parkingSlot.id, newEndStop)
+
+        val returnValue = Database.incrementOccupation(parkingSlotTestCollection, incrementOccupation, parkingSlotList)
+
+        assertEquals(false, returnValue)
+    }
+
 }
