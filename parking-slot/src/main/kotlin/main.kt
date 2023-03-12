443580fc1dfa8fc0e8adb962572cca5fb78c9ffc
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-03-12 10:35:42 +0100

    build(parkingslot): moved ktor client dependencies to test implementation

--- parking-slot/src/main/kotlin/main.kt
+++ parking-slot/src/main/kotlin/main.kt
@@ -22,5 +22,5 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 fun main() {
-    FillParkingSlotCollection.eraseAndFillCollection("parking-slot-test")
+    FillParkingSlotCollection.eraseAndFillCollection("parking-slot-test", 10)
 }
