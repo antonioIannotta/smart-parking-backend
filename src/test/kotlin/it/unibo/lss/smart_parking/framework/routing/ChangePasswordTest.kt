d5261a3c96f09414a086a1718e53835d06215ce1
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2023-05-09 08:43:57 +0200

    fix: pass encoding secrets to test methods

--- src/test/kotlin/it/unibo/lss/smart_parking/framework/routing/ChangePasswordTest.kt
+++ src/test/kotlin/it/unibo/lss/smart_parking/framework/routing/ChangePasswordTest.kt
@@ -6,12 +6,12 @@ import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
 import io.ktor.test.dispatcher.*
-import it.unibo.lss.smart_parking.entity.UserCredentials
 import it.unibo.lss.smart_parking.framework.module
 import it.unibo.lss.smart_parking.framework.utils.getUserCollection
 import it.unibo.lss.smart_parking.framework.utils.getUserMongoClient
 import it.unibo.lss.smart_parking.interface_adapter.UserInterfaceAdapter
 import it.unibo.lss.smart_parking.interface_adapter.model.request.ChangePasswordRequestBody
+import it.unibo.lss.smart_parking.interface_adapter.model.request.LoginRequestBody
 import it.unibo.lss.smart_parking.interface_adapter.model.request.SignUpRequestBody
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -19,17 +19,18 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class ChangePasswordTest {
 
     companion object {
+        private const val testSecret = "1234567890"
         private lateinit var testApp: TestApplication
-        private var interfaceAdapter = UserInterfaceAdapter(getUserCollection(getUserMongoClient()))
+        private var interfaceAdapter = UserInterfaceAdapter(getUserCollection(getUserMongoClient()), testSecret)
         private const val changePasswordEndpoint = "/user/change-password"
         private const val testMail = "test@test.it"
         private const val testPassword = "Test123!"
         private const val testName = "testName"
-        private const val testSecret = "1234567890"
 
         private lateinit var userId: String
 
@@ -38,7 +39,7 @@ class ChangePasswordTest {
         fun config() {
             testApp = TestApplication {
                 application {
-                    module(testSecret)
+                    module(testSecret, testSecret)
                 }
                 //register test user
                 val signUpRequestBody = SignUpRequestBody(testMail, testPassword, testName)
@@ -62,8 +63,10 @@ class ChangePasswordTest {
     @Test
     fun `test that change-password api let a user to change his password`() = testSuspend {
         //get a valid jwt for the user and prepare request body
-        val credentials = UserCredentials(testMail, testPassword)
-        val jwt = interfaceAdapter.login(credentials, testSecret).token
+        val loginResult = interfaceAdapter.login(LoginRequestBody(testMail, testPassword), testSecret)
+        val userId = loginResult.userId
+        val jwt = loginResult.token
+        assertNotNull(userId)
         val newPassword = "abcde"
         val changePasswordRequestBody = ChangePasswordRequestBody(testPassword, newPassword)
         //change user's password
@@ -79,8 +82,7 @@ class ChangePasswordTest {
             //response verification
             assertEquals(HttpStatusCode.OK, call.response.status)
             //new password verification
-            val newCredentials = UserCredentials(testMail, newPassword)
-            assertEquals(true, interfaceAdapter.validateCredentials(newCredentials))
+            assertEquals(true, interfaceAdapter.validateCredentials(userId, newPassword))
         }
     }
 
